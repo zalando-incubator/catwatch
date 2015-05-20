00a7f13ac2dbbd7449fd0ce260a21448c67b73e9
Joern Horstmann <joern.horstmann@zalando.de>
2015-05-20 20:36:52 +0200

    Return one project for testing

--- birdwatch/api.py
+++ birdwatch/api.py
@@ -1,8 +1,22 @@
 __author__ = 'jloeffler'
 
+from github3 import GitHub
+from github3.models import GitHubError
+from github3.repos.repo import Repository
+from birdwatch.configuration import configuration
+from birdwatch.collector import Project
+
 def list_projects():
 
-    return {}
+    # just for testing
+    github = GitHub(token=configuration.github_token)
+    zalando_repos = github.iter_user_repos("zalando")
+
+    projects = {'projects': [{'name': repo.name} for repo in zalando_repos if repo.name == 'birdwatch']}
+
+    print(projects)
+
+    return projects
 
 def list_contributors():
 
