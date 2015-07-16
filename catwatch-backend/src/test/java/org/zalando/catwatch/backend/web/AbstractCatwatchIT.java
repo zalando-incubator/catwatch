4ffcaadfa2eb28bd433feade936c2458aaf57050
rwitzel <rodrigo.witzel@zalando.de>
2015-07-16 11:23:00 +0200

    refactored: duplicated code in integration tests moved to abstract super class

---
+++ catwatch-backend/src/test/java/org/zalando/catwatch/backend/web/AbstractCatwatchIT.java
@@ -0,0 +1,34 @@
+package org.zalando.catwatch.backend.web;
+
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+import org.zalando.catwatch.backend.CatWatchBackendApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CatWatchBackendApplication.class)
+@WebIntegrationTest
+@IntegrationTest({ "server.port=0" })
+public class AbstractCatwatchIT {
+
+	@Value("${local.server.port}")
+	private int port;
+
+	protected URL base;
+	
+	protected RestTemplate template;
+
+	@Before
+	public void setUp() throws Exception {
+		this.base = new URL("http://localhost:" + port + "/");
+		template = new TestRestTemplate();
+	}
+}
--- catwatch-backend/src/test/java/org/zalando/catwatch/backend/web/config/CorsFilterIT.java
+++ catwatch-backend/src/test/java/org/zalando/catwatch/backend/web/config/CorsFilterIT.java
@@ -4,39 +4,12 @@ import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-import java.net.URL;
-
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.client.RestTemplate;
-import org.zalando.catwatch.backend.CatWatchBackendApplication;
+import org.zalando.catwatch.backend.web.AbstractCatwatchIT;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = CatWatchBackendApplication.class)
-@WebIntegrationTest
-@IntegrationTest({ "server.port=0" })
-public class CorsFilterIT {
-
-	@Value("${local.server.port}")
-	private int port;
-
-	private URL base;
-	private RestTemplate template;
-
-	@Before
-	public void setUp() throws Exception {
-		this.base = new URL("http://localhost:" + port + "/");
-		template = new TestRestTemplate();
-	}
+public abstract class CorsFilterIT extends AbstractCatwatchIT {
 
 	@Test
 	public void testCorsHeadersInResponse_forRootPage() throws Exception {
