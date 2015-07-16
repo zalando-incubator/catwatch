c39af9969d675297d9d11bfcefedbbed0b3d379b
rwitzel <rodrigo.witzel@zalando.de>
2015-07-16 11:53:58 +0200

    changed: primary key of Statistics from long to composite key

--- catwatch-backend/src/main/java/org/zalando/catwatch/backend/repo/StatisticsRepository.java
+++ catwatch-backend/src/main/java/org/zalando/catwatch/backend/repo/StatisticsRepository.java
@@ -2,7 +2,8 @@ package org.zalando.catwatch.backend.repo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.zalando.catwatch.backend.model.Statistics;
+import org.zalando.catwatch.backend.model.StatisticsKey;
 
-public interface StatisticsRepository extends CrudRepository<Statistics, Long> {
+public interface StatisticsRepository extends CrudRepository<Statistics, StatisticsKey> {
 
 }
