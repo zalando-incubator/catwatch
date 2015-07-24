832e9da1c4df3c796eca352753c44e757b716f4e
Manuel Kunz <manuel.kunz@zalando.de>
2015-07-24 15:59:56 +0200

    changed projects data model

--- catwatch-backend/src/main/java/org/zalando/catwatch/backend/repo/populate/ProjectBuilder.java
+++ catwatch-backend/src/main/java/org/zalando/catwatch/backend/repo/populate/ProjectBuilder.java
@@ -11,48 +11,52 @@ import org.zalando.catwatch.backend.repo.ProjectRepository;
 
 public class ProjectBuilder {
 
-	private Project project;
+    private Project project;
 
-	private ProjectRepository projectRepository;
+    private ProjectRepository projectRepository;
 
-	public ProjectBuilder(ProjectRepository projectRepository) {
-		this.projectRepository = projectRepository;
-		project = new Project(freshId(), randomDate());
-		project.setName(randomProjectName());
-		project.setPrimaryLanguage(randomLanguage());
-		project.setForksCount(random(1, 10));
-		project.setStarsCount(random(1, 4));
-	}
+    public ProjectBuilder(final ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+        project = new Project();
+        project.setGitHubProjectId(freshId());
+        project.setSnapshotDate(randomDate());
+        project.setName(randomProjectName());
+        project.setPrimaryLanguage(randomLanguage());
+        project.setForksCount(random(1, 10));
+        project.setStarsCount(random(1, 4));
+    }
 
-	private void updateUrl() {
-		project.setUrl("https://github.com/" + project.getOrganizationName() + "/" + project.getName());
-	}
+    private void updateUrl() {
+        project.setUrl("https://github.com/" + project.getOrganizationName() + "/" + project.getName());
+    }
 
-	public ProjectBuilder name(String name) {
-		project.setName(name);
-		return this;
-	}
+    public ProjectBuilder name(final String name) {
+        project.setName(name);
+        return this;
+    }
 
-	public ProjectBuilder organizationName(String organizationName) {
-		project.setOrganizationName(organizationName);
-		return this;
-	}
+    public ProjectBuilder organizationName(final String organizationName) {
+        project.setOrganizationName(organizationName);
+        return this;
+    }
 
-	public Project create() {
-		
-		updateUrl();
-		
-		Project p = new Project(project.getId(), project.getSnapshotDate());
-		p.setName(project.getName());
-		p.setPrimaryLanguage(project.getPrimaryLanguage());
-		p.setForksCount(project.getForksCount());
-		p.setStarsCount(project.getStarsCount());
-		p.setOrganizationName(project.getOrganizationName());
-		return p;
-	}
+    public Project create() {
 
-	public Project save() {
-		return projectRepository.save(create());
-	}
+        updateUrl();
+
+        Project p = new Project();
+        p.setGitHubProjectId(project.getGitHubProjectId());
+        p.setSnapshotDate(project.getSnapshotDate());
+        p.setName(project.getName());
+        p.setPrimaryLanguage(project.getPrimaryLanguage());
+        p.setForksCount(project.getForksCount());
+        p.setStarsCount(project.getStarsCount());
+        p.setOrganizationName(project.getOrganizationName());
+        return p;
+    }
+
+    public Project save() {
+        return projectRepository.save(create());
+    }
 
 }
--- catwatch-backend/src/test/java/org/zalando/catwatch/backend/scheduler/TaskSchedulerTest.java
+++ catwatch-backend/src/test/java/org/zalando/catwatch/backend/scheduler/TaskSchedulerTest.java
@@ -1,8 +1,3 @@
 package org.zalando.catwatch.backend.scheduler;
 
-import org.junit.runner.RunWith;
-
-import org.mockito.runners.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
 public class TaskSchedulerTest { }
