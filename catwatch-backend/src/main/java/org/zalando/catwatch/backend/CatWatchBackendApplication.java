036ab3c42d22ced503c73a992cd37e3f5463d2e7
rwitzel <rodrigo.witzel@zalando.de>
2015-07-27 16:22:02 +0200

    activated: scheduling

--- catwatch-backend/src/main/java/org/zalando/catwatch/backend/CatWatchBackendApplication.java
+++ catwatch-backend/src/main/java/org/zalando/catwatch/backend/CatWatchBackendApplication.java
@@ -2,8 +2,10 @@ package org.zalando.catwatch.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CatWatchBackendApplication {
 
 	public static void main(String[] args) {
