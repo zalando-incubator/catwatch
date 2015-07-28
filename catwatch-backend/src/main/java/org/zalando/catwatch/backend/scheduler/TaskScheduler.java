3dccf8b3ecb2112b6f3a91d0f52ddd408f6122f7
rwitzel <rodrigo.witzel@zalando.de>
2015-07-28 18:39:47 +0200

    refactored: the schedule pattern for the fetch task can be configured via the command line now improved: if an exception occurs during the "login", then the exception is logged (and a better exception message is shown) moved: the @Scheduled annotation to a new class because the scheduler should not be configured for tests (and the expression in the pattern does not work at the moment. don't know the reason) removed: empty test class

--- catwatch-backend/src/main/java/org/zalando/catwatch/backend/scheduler/TaskScheduler.java
+++ catwatch-backend/src/main/java/org/zalando/catwatch/backend/scheduler/TaskScheduler.java
@@ -1,80 +1,22 @@
 package org.zalando.catwatch.backend.scheduler;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.*;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.zalando.catwatch.backend.github.Snapshot;
-import org.zalando.catwatch.backend.github.SnapshotProvider;
-import org.zalando.catwatch.backend.repo.ContributorRepository;
-import org.zalando.catwatch.backend.repo.ProjectRepository;
-import org.zalando.catwatch.backend.repo.StatisticsRepository;
 
 @Component
+@Profile("-test")
 public class TaskScheduler {
 
-    private static final Logger logger = LoggerFactory.getLogger(TaskScheduler.class);
+	@Autowired
+	private Fetcher fetcher;
 
-    @Autowired
-    private ProjectRepository projectRepository;
-
-    @Autowired
-    private StatisticsRepository statisticsRepository;
-
-    @Autowired
-    private ContributorRepository contributorRepository;
-
-    @Autowired
-    private SnapshotProvider snapshotProvider;
-
-    @SuppressWarnings("MismatchedReadAndWriteOfArray")
-    @Value("#{'${organization.list}'.split(',')}")
-    private String[] organizations;
-
-    /**
-     * This is used to fetch every Organization statistics from GitHub
-     * (This runs at 8 AM everyday)
-     */
-    @Scheduled(cron = "0 17 * * * ?")
-    public void fetchData() {
-        logger.info("Starting fetching data.");
-
-        List<Future<Snapshot>> futures = new ArrayList<>();
-
-        try {
-            for (String organizationName : organizations) {
-                futures.add(snapshotProvider.takeSnapshot(organizationName));
-                logger.info("Enqueued task TakeSnapshotTask for organization '{}'", organizationName);
-            }
-        } catch (IOException e) {
-            logger.error("No GitHub API credentials found. Unable to fetch data from GitHub API.");
-            return;
-        }
-        logger.info("Submitted {} TakeSnapshotTasks.", futures.size());
-
-        for (Future<Snapshot> future : futures) {
-            try {
-                Snapshot snapshot = future.get();
-                logger.info("Successfully fetched data for organization '{}'", snapshot.getStatistics().getOrganizationName());
-
-                statisticsRepository.save(snapshot.getStatistics());
-                projectRepository.save(snapshot.getProjects());
-                contributorRepository.save(snapshot.getContributors());
-                // TODO languagesRepository.save(snapshot.getLanguages());
-
-                logger.info("Successfully saved data for organization '{}'", snapshot.getStatistics().getOrganizationName());
-            } catch (InterruptedException | ExecutionException e) {
-                logger.error("Error occurred while processing organization.", e);
-            }
-        }
-
-        logger.info("Finished fetching data.");
-    }
+	/**
+	 * This is used to fetch every Organization statistics from GitHub.
+	 */
+	@Scheduled(cron = "${schedule}")
+	public void fetchData() {
+		fetcher.fetchData();
+	}
 }
--- catwatch-backend/src/main/java/org/zalando/catwatch/backend/web/fetch/FetchController.java
+++ catwatch-backend/src/main/java/org/zalando/catwatch/backend/web/fetch/FetchController.java
@@ -5,18 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.zalando.catwatch.backend.scheduler.Fetcher;
 import org.zalando.catwatch.backend.scheduler.TaskScheduler;
 
 @Controller
 public class FetchController {
 
 	@Autowired
-	private TaskScheduler taskScheduler;
+	private Fetcher fetcher;
 
 	@RequestMapping(value = "/fetch", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
 	@ResponseBody
 	public String fetch() throws Exception {
-		taskScheduler.fetchData();
+		fetcher.fetchData();
 		return "OK";
 	}
 
