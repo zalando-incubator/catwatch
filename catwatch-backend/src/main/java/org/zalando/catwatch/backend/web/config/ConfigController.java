c6dcf689ccad6140a8d71994aa83e9b813f4913e
Rodrigo Witzel <rodrigo.witzel@gmx.de>
2015-07-29 00:04:01 +0200

    improved ConfigController so that a number of important configuration options are listed

--- catwatch-backend/src/main/java/org/zalando/catwatch/backend/web/config/ConfigController.java
+++ catwatch-backend/src/main/java/org/zalando/catwatch/backend/web/config/ConfigController.java
@@ -1,10 +1,12 @@
 package org.zalando.catwatch.backend.web.config;
 
-import static java.util.Collections.singletonMap;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import java.util.Map;
+import java.util.Properties;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -15,14 +17,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ConfigController {
 
-	@Autowired
-	private Environment env;
+    @Autowired
+    private Environment env;
 
-	@RequestMapping(value = "/config", method = GET, produces = { APPLICATION_JSON_VALUE })
-	@ResponseBody
-	public Map<String, String> config() {
-		String orgaListParam = "organization.list";
-		return singletonMap(orgaListParam, env.getProperty(orgaListParam));
-	}
+    @RequestMapping(value = "/config", method = GET, produces = { APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public Map<String, String> config() throws Exception {
+        Properties properties = new Properties();
+        properties.load(getClass().getResourceAsStream("/application-public.properties"));
+        
+        SortedMap<String,  String> result = new TreeMap<>();
+        properties.stringPropertyNames().stream().forEach(name -> result.put(name, env.getProperty(name)));
+        return result;
+    }
 
 }
