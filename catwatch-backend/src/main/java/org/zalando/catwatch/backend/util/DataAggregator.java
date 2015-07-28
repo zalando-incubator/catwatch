88df4daade121d651f9f198b296b260a3d9369ca
Frank Schulze <frank.schulze@zalando.de>
2015-07-28 18:49:59 +0200

    added Language endpoint controller and integration test (not yet finished)

--- catwatch-backend/src/main/java/org/zalando/catwatch/backend/util/DataAggregator.java
+++ catwatch-backend/src/main/java/org/zalando/catwatch/backend/util/DataAggregator.java
@@ -3,12 +3,20 @@ package org.zalando.catwatch.backend.util;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.zalando.catwatch.backend.model.Language;
+import org.zalando.catwatch.backend.model.Project;
 import org.zalando.catwatch.backend.model.Statistics;
+import org.zalando.catwatch.backend.repo.ProjectRepository;
 
 public class DataAggregator {
 
@@ -130,5 +138,61 @@ public class DataAggregator {
 		return tempSum;
 	}
 	
+	
+    public static List<Language> filterLanguages(List<Language> languages, int limit,  int offset){
+    	
+
+         return  languages.stream().skip(offset).limit(limit).collect(Collectors.toList());
+
+    }
+    
+    
+    public static List<Language> getMainLanguages(final String organizations, final Comparator<Language> c, ProjectRepository repository, Optional<String> filterLanguage) {
+
+        Collection<String> organizationList = StringParser.parseStringList(organizations, ",");
+        List<Project> projectList = new ArrayList<>();
+
+        // get the projects
+        for (String org : organizationList) {
+
+            Iterable<Project> projects = repository.findProjects(org, Optional.ofNullable(null), filterLanguage);
+
+            Iterator<Project> iter = projects.iterator();
+            while (iter.hasNext()) {
+                projectList.add(iter.next());
+            }
+        }
+
+        // count the languages
+
+        List<String> languageList = new ArrayList<>();
+
+        for (Project p : projectList) {
+            languageList.add(p.getPrimaryLanguage());
+        }
+
+        List<Language> languages = new ArrayList<>();
+
+        Set<String> languageSet = new HashSet<>(languageList);
+
+        int frequency = 0;
+
+        for (String language : languageSet) {
+            Language l = new Language(language);
+            frequency = Collections.frequency(languageList, language);
+
+            l.setPercentage((int) Math.round(((double) frequency) / languageList.size() * 100));
+            l.setProjectsCount(frequency);
+
+            languages.add(l);
+        }
+
+        // sort
+        if (languages.size() > 1) {
+            Collections.sort(languages, c);
+        }
+
+        return languages;
+    }
 	
 }
--- catwatch-backend/src/test/java/org/zalando/catwatch/backend/web/LanguagesControllerIT.java
+++ catwatch-backend/src/test/java/org/zalando/catwatch/backend/web/LanguagesControllerIT.java
@@ -5,76 +5,144 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.ResponseEntity;
+import org.zalando.catwatch.backend.model.Language;
+import org.zalando.catwatch.backend.model.Project;
+import org.zalando.catwatch.backend.model.Statistics;
 import org.zalando.catwatch.backend.repo.ProjectRepository;
 import org.zalando.catwatch.backend.repo.populate.BuilderUtil;
 import org.zalando.catwatch.backend.repo.populate.ProjectBuilder;
+import org.zalando.catwatch.backend.util.TestUtils;
 
 public class LanguagesControllerIT extends AbstractCatwatchIT {
 
-    private final String LANG_JAVA = "Java", LANG_JS = "JS", LANG_HTML = "HTML5", LANG_CSS = "CSS", LANG_PY = "Python",
-        LANG_CPP = "C++", LANG_GO = "Go", LANG_SCALA = "Scala", LANG_GROOVY = "Groovy", LANG_CSH = "C#",
-        LANG_CLO = "Clojure", LANG_VB = "VB", LANG_OC = "OjectiveC";
+	
+	private final String 
+			ORG1= "organization1",
+			ORG2 = "organization2";
+	@Autowired
+	ProjectRepository repository;
 
-    @Autowired
-    ProjectRepository repository;
+	@Test
+	public void testGetLanguages() {
 
-    @Test
-    public void testgetLanguages() {
+		// given
+		this.repository.deleteAll();
+		fillRepositoryRandom(6, ORG2);
+		List<String> languageNames = fillRepositoryRandom(40, ORG1);
+		
+		List<Language> sortedLanguages = generateLanguageList(languageNames);
 
-        // when
-        this.repository.deleteAll();
+		// when
+		String url = TestUtils.createAbsoluteStatisticsUrl(this.base.toString(), ORG1, null, null);
 
-        List<String> languages = fillRepositoryRandom(10);
+		ResponseEntity<Language[]> response = template.getForEntity(url, Language[].class);
 
-// Collections.sort TODO
+		Language[] statsResponse = response.getBody();
 
-    }
+		// then
+		//FIXME Assert.assertThat(statsResponse.length, Matchers.equalTo(sortedLanguages.size()));
+	}
 
-    @Test
-    public void testGetLanguagesWithOffsetAndLimit() { }
+	
+	@Test
+	public void testGetLanguagesWithOffsetAndLimit() {
+	}
 
-    private List<String> fillRepositoryRandom(final int nrOfProjects) {
+	
+	
+	private List<String> fillRepositoryRandom(final int nrOfProjects, String organization) {
 
-        List<String> languages = new ArrayList<>();
+		
+		System.out.println("Creating projects");
+		
+		List<String> languages = new ArrayList<>();
+		
+		for (int i = 0; i < nrOfProjects; i++) {
+			Project p = new ProjectBuilder(repository, new Date(), 0L, null, null, 0, 0, 0, 0, 0)
+					.primaryLanguage(BuilderUtil.randomLanguage())
+					.organizationName(organization)
+					.name("p"+i)
+					.save();
+			
+			System.out.println("Stored project with name "+p.getName());
+			
+			languages.add(p.getPrimaryLanguage());
+		}
+		
+//		FIXME Assert.assertEquals(nrOfProjects, repository.findProjects(organization, Optional.ofNullable(null), Optional.ofNullable(null)).size());
 
-        String lang;
+		return languages;
+	}
 
-        for (int i = 0; i < nrOfProjects; i++) {
-            lang = BuilderUtil.randomLanguage();
-            new ProjectBuilder(repository, new Date(), 0L, null, null, 0, 0, 0, 0, 0).primaryLanguage(lang).save();
-            languages.add(lang);
-        }
+	private class LanguageComparator implements Comparator<Language> {
 
-        return languages;
-    }
+		Collection<String> stringCollection;
 
-    private class LanguageComparator implements Comparator<String> {
+		public LanguageComparator(final Collection<String> languageNames) {
+			this.stringCollection = languageNames;
+		}
 
-        Collection<String> stringCollection;
+		@Override
+		public int compare(final Language o1, final Language o2) {
 
-        public LanguageComparator(final Collection<String> strings) {
-            this.stringCollection = strings;
-        }
+			int f1 = Collections.frequency(stringCollection, o1.getName());
 
-        @Override
-        public int compare(final String o1, final String o2) {
+			int f2 = Collections.frequency(stringCollection, o2.getName());
 
-            int f1 = Collections.frequency(stringCollection, o1);
+			if (f1 >= f2) {
+				return -1;
+			}
 
-            int f2 = Collections.frequency(stringCollection, o2);
+			return 1;
+		}
 
-            if (f1 >= f2) {
-                return 1;
-            }
+	}
 
-            return -1;
-        }
+	private List<Language> generateLanguageList(List<String> languageNames) {
 
-    }
+		List<Language> languages;
+		Map<String, Integer> languageCount = new HashMap<>();
+
+		Iterator<String> iter = languageNames.iterator();
+
+		while (iter.hasNext()) {
+
+			String langName = iter.next();
+
+			if (!languageCount.keySet().contains(langName)) {
+				languageCount.put(langName, 1);
+			} else {
+				int old = languageCount.get(langName);
+				languageCount.put(langName, ++old);
+			}
+		}
+
+		languages = new ArrayList<>();
+
+		for (String langName : languageCount.keySet()) {
+			Language lang = new Language(langName);
+
+			lang.setProjectsCount(languageCount.get(langName));
+
+			lang.setPercentage((int) Math.round((double) lang.getProjectsCount() / languageNames.size() * 100));
+
+			languages.add(lang);
+
+		}
+
+		Collections.sort(languages, new LanguageComparator(languageNames));
+
+		return languages;
+	}
 }
