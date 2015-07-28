60cd5859a2109450d56c7830d2a48925a40e7363
Roman Bobin <roman.bobin@zalando.de>
2015-07-28 13:16:34 +0200

    fixed ConfigController test

--- catwatch-backend/src/test/java/org/zalando/catwatch/backend/web/ConfigControllerIT.java
+++ catwatch-backend/src/test/java/org/zalando/catwatch/backend/web/ConfigControllerIT.java
@@ -4,14 +4,18 @@ import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 
 public class ConfigControllerIT extends AbstractCatwatchIT {
+
+	@Value("${organization.list}")
+	String organizationList;
 
 	@Test
 	public void testConfigOutput() throws Exception {
 		ResponseEntity<String> response = template.getForEntity(base.toString() + "/config", String.class);
 		assertThat(response.getBody(), containsString("organization.list"));
-		assertThat(response.getBody(), containsString("zalando")); // default value
+		assertThat(response.getBody(), containsString(organizationList));
 	}
 }
