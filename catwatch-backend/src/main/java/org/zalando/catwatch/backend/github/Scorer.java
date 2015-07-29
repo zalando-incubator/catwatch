745238a600283f482db015e922f93198fff3e83b
Rodrigo Witzel <rodrigo.witzel@gmx.de>
2015-07-29 22:41:10 +0200

    refactored: Scorer (separated domain logic from generic util code)

--- catwatch-backend/src/main/java/org/zalando/catwatch/backend/github/Scorer.java
+++ catwatch-backend/src/main/java/org/zalando/catwatch/backend/github/Scorer.java
@@ -1,12 +1,7 @@
 package org.zalando.catwatch.backend.github;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import javax.script.Bindings;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import static org.zalando.catwatch.backend.util.JavaScriptExecutor.newExecutor;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -19,20 +14,12 @@ public class Scorer {
     private String scoringProject;
 
     public int score(Project project) {
-
-        ScriptEngineManager mgr = new ScriptEngineManager();
-        ScriptEngine engine = mgr.getEngineByName("JavaScript");
-        try {
-            Bindings bindings = engine.createBindings();
-            Map<String, Object> map = new HashMap<String, Object>();
-            map.put("project", project);
-            bindings.put("map", map);
-            String js = "var scoring = " + scoringProject + ";\n map.score = scoring(map.project);";
-            engine.eval(js, bindings);
-            return ((Number) map.get("score")).intValue();
-        } catch (ScriptException e) {
-            throw new RuntimeException("never to happen: " + e.getMessage(), e);
-        }
+        
+        String jsCode = "";
+        jsCode += "var scoring = " + scoringProject + ";\n";
+        jsCode += "result.value = scoring(project);";
+        
+        return ((Number) newExecutor(jsCode).bind("project", project).execute()).intValue();
     }
 
     public void setScoringProject(String scoringProject) {
