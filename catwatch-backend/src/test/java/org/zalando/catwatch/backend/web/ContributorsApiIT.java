7bfbddaff6878b30cf58734439069ff4412efc4f
rwitzel <rodrigo.witzel@zalando.de>
2015-07-29 22:08:06 +0200

    simplified/improved: test code for Contributor

--- catwatch-backend/src/test/java/org/zalando/catwatch/backend/web/ContributorsApiIT.java
+++ catwatch-backend/src/test/java/org/zalando/catwatch/backend/web/ContributorsApiIT.java
@@ -5,6 +5,7 @@ import static java.time.temporal.ChronoUnit.DAYS;
 import static java.time.temporal.ChronoUnit.HOURS;
 import static java.util.Date.from;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.stringContainsInOrder;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
@@ -45,11 +46,10 @@ public class ContributorsApiIT extends AbstractCatwatchIT {
 
         // given
         repository.deleteAll();
-        Contributor c = newContributor().id(11).days(2).organizationId(1).organizationName("a").orgCommits(28).save();
-        assertNotNull(c.getSnapshotDate());
+        Contributor c = newContributor().organizationName("IBM").save();
 
         // when
-        String url = contributorUrl().queryParam("organizations", "a").toUriString();
+        String url = contributorUrl().queryParam("organizations", "IBM").toUriString();
         Map<String, Object>[] contributors = template.getForEntity(url, Map[].class).getBody();
 
         // then
@@ -64,24 +64,24 @@ public class ContributorsApiIT extends AbstractCatwatchIT {
         // given
         repository.deleteAll();
 
-        newContributor().id(11).days(2).organizationId(1).organizationName("a").orgCommits(28).save();
-        newContributor().id(11).days(3).organizationId(1).organizationName("a").orgCommits(23).save();
-        newContributor().id(11).days(4).organizationId(1).organizationName("a").orgCommits(20).save();
+        newContributor().id(11).days(2).organizationId(1).organizationName("IBM").orgCommits(28).save();
+        newContributor().id(11).days(3).organizationId(1).organizationName("IBM").orgCommits(23).save();
+        newContributor().id(11).days(4).organizationId(1).organizationName("IBM").orgCommits(20).save();
 
-        newContributor().id(12).days(2).organizationId(2).organizationName("b").orgCommits(18).save();
-        newContributor().id(12).days(4).organizationId(2).organizationName("b").orgCommits(16).save();
+        newContributor().id(12).days(2).organizationId(2).organizationName("Sun").orgCommits(18).save();
+        newContributor().id(12).days(4).organizationId(2).organizationName("Sun").orgCommits(16).save();
 
-        newContributor().id(13).days(2).organizationId(1).organizationName("a").orgCommits(7).save();
-        newContributor().id(13).days(4).organizationId(1).organizationName("a").orgCommits(4).save();
+        newContributor().id(13).days(2).organizationId(1).organizationName("IBM").orgCommits(7).save();
+        newContributor().id(13).days(4).organizationId(1).organizationName("IBM").orgCommits(4).save();
 
         Date endDate = from(now());
-        Date startDate = from(Instant.now().minus(3, DAYS).minus(12, HOURS));
+        Date startDate = from(now().minus(3, DAYS).minus(12, HOURS));
 
         // when
         // (TODO does not work with " a, b " yet -> "%20a,b")
         String url = contributorUrl()
                 //
-                .queryParam(API_REQUEST_PARAM_ORGANIZATIONS, "a,b")
+                .queryParam(API_REQUEST_PARAM_ORGANIZATIONS, "IBM,Sun")
                 .queryParam(API_REQUEST_PARAM_STARTDATE, iso8601(startDate)) //
                 .queryParam(API_REQUEST_PARAM_ENDDATE, iso8601(endDate)) //
                 .toUriString();
@@ -100,8 +100,6 @@ public class ContributorsApiIT extends AbstractCatwatchIT {
         assertThat(contributors[2].getId(), equalTo(12L));
         assertThat(contributors[2].getOrganizationalCommitsCount(), equalTo(2));
         
-        assertThat(contributors[0].toString(), Matchers.stringContainsInOrder(Arrays.asList("id: "+11)));
-
         assertThat(contributors.length, equalTo(3));
     }
 
