e383b98090fb22c3e47609079d4216387926e62f
Roman Bobin <roman.bobin@zalando.de>
2015-07-29 10:35:12 +0200

    fixed tests

--- catwatch-backend/src/main/java/org/zalando/catwatch/backend/github/SnapshotProvider.java
+++ catwatch-backend/src/main/java/org/zalando/catwatch/backend/github/SnapshotProvider.java
@@ -8,6 +8,7 @@ import org.kohsuke.github.GitHubBuilder;
 import org.kohsuke.github.extras.OkHttpConnector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
