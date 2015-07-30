674a65eda76ae3042cdde4413f8a80035cb31fd0
Manuel Kunz <manuel.kunz@zalando.de>
2015-07-30 15:00:38 +0200

    added zalando aws endpoint

--- catwatch-dump/import.sh
+++ catwatch-dump/import.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-importurl=http://ec2-52-28-130-84.eu-central-1.compute.amazonaws.com/import
+#importurl=http://ec2-52-28-130-84.eu-central-1.compute.amazonaws.com/import
+importurl=https://catwatch-web.hackweek.zalan.do/import
 
 echo "file $1 will be imported to $importurl ..."
 # I guess it won't work with UTF-8
-curl -H "Content-Type: application/json; charset=ISO-8859-1" -X POST -d @$1 $importurl
+curl --insecure -H "Content-Type: application/json; charset=ISO-8859-1" -X POST -d @$1 $importurl
 
 echo ""
 echo "import finished"
