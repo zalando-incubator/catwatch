25af0fdef5d71785ffd59bff3d53d3c682b4db10
Mikio Braun <mikiobraun@gmail.com>
2015-08-12 15:11:02 +0200

    now uses properly formatted date time string

---
+++ catwatch-backend/src/main/java/org/zalando/catwatch/backend/util/JsonDateListSerializer.java
@@ -0,0 +1,28 @@
+package org.zalando.catwatch.backend.util;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by mibraun on 12/08/15.
+ */
+public class JsonDateListSerializer extends JsonSerializer<List<Date>> {
+
+    @Override
+    public void serialize(List<Date> dates, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        jsonGenerator.writeStartArray();
+        for (Date date: dates) {
+            String formattedDate = format.format(date);
+            jsonGenerator.writeString(formattedDate);
+        }
+        jsonGenerator.writeEndArray();
+    }
+}
