87da8a64b169af97b1058f13c2162956df9bb8e5
Rodrigo Witzel <rodrigo.witzel@gmx.de>
2015-08-22 11:13:49 +0200

    added: generation of test dependencies (useful to maintain the gradle build)

--- catwatch-backend/etc/dependencies/generate-dependency-info.sh
+++ catwatch-backend/etc/dependencies/generate-dependency-info.sh
@@ -2,8 +2,13 @@
 rm -f generated-runtime-deps-list.txt
 rm -f generated-runtime-deps-tree.txt
 
+rm -f generated-test-deps-list.txt
+rm -f generated-test-deps-tree.txt
+
 cd ../..
 
 mvn dependency:list -DincludeScope=runtime -DoutputFile=etc/dependencies/generated-runtime-deps-list.txt -Dsort=true
 mvn dependency:tree -Dscope=runtime -DoutputFile=etc/dependencies/generated-runtime-deps-tree.txt
- 
+
+mvn dependency:list -DincludeScope=test -DoutputFile=etc/dependencies/generated-test-deps-list.txt -Dsort=true
+mvn dependency:tree -Dscope=test -DoutputFile=etc/dependencies/generated-test-deps-tree.txt
